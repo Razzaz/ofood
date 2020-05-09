a54d77bae94feb2fe56a2aa6e2fba20384e20ee8
Ridho Gani <ridhoadhadigani@gmail.com>
2020-05-09 19:16:29 +0700

    price database

--- app/src/main/java/com/example/ofood/ItemActivity.java
+++ app/src/main/java/com/example/ofood/ItemActivity.java
@@ -1,17 +1,32 @@
 package com.example.ofood;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
-import android.content.ClipData;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import java.util.Objects;
 
 public class ItemActivity extends AppCompatActivity {
+
+    int[] itemImages = {R.drawable.item_eggplant, R.drawable.item_carrot, R.drawable.item_tomato, R.drawable.item_potato, R.drawable.item_broccoli, R.drawable.item_lettuce};
+    String[] foodNames = {"Eggplant", "Carrot", "Tomato", "Potato", "Brocoli", "Lettuce"};
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    private TextView mItemPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +41,43 @@ public class ItemActivity extends AppCompatActivity {
         catch (NullPointerException ignored){}
 
         setContentView(R.layout.activity_item);
+
+        String foodName = getIntent().getStringExtra("foodName");
+        String itemDescription = getIntent().getStringExtra("itemDescription");
+
+        TextView mFoodName = findViewById(R.id.foodName);
+        TextView mItemName = findViewById(R.id.itemName);
+        mItemPrice = findViewById(R.id.itemPrice);
+        TextView mItemDescription = findViewById(R.id.itemDescription);
+        ImageView mItemImage = findViewById(R.id.itemImage);
+
+        mFoodName.setText(foodName);
+        mItemName.setText(foodName);
+        DocumentReference vegetable = db.collection("Vegetables").document(foodName);
+        vegetable.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    return;
+                }
+
+                if (documentSnapshot != null && documentSnapshot.exists()) {
+                    String price = documentSnapshot.getString("Price");
+                    mItemPrice.setText(String.format("Rp.%s/Kg", price));
+                }
+                else {
+                    mItemPrice.setText("");
+                }
+            }
+        });
+
+        mItemDescription.setText(itemDescription);
+        assert foodName != null;
+        for(int i = 0; i < foodNames.length; i++){
+            if(foodName.equals(foodNames[i])){
+                mItemImage.setImageResource(itemImages[i]);
+            }
+        }
     }
 
     public void setTransparentStatusBarOnly(Activity activity) {
--- app/src/main/java/com/example/ofood/StoreActivity.java
+++ app/src/main/java/com/example/ofood/StoreActivity.java
@@ -1,5 +1,6 @@
 package com.example.ofood;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -9,6 +10,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import java.util.Objects;
 
@@ -29,13 +36,77 @@ public class StoreActivity extends AppCompatActivity {
         setContentView(R.layout.activity_store);
 
         Button buttonBroccoli = findViewById(R.id.buttonBroccoli);
+        Button buttonTomato = findViewById(R.id.buttonTomato);
+        Button buttonPotato = findViewById(R.id.buttonPotato);
+        Button buttonCarrot = findViewById(R.id.buttonCarrot);
+        Button buttonLettuce = findViewById(R.id.buttonLettuce);
+        Button buttonEggplant = findViewById(R.id.buttonEggplant);
+
         buttonBroccoli.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), ItemActivity.class));
+                Intent intent = new Intent(StoreActivity.this, ItemActivity.class);
+                intent.putExtra("foodName","Brocoli");
+                intent.putExtra("itemPrice", 14000);
+                intent.putExtra("itemDescription", "Herbaceous annual or biennial grown for its edible flower heads which are used as a vegetable.");
+                startActivity(intent);
             }
         });
 
+        buttonTomato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StoreActivity.this, ItemActivity.class);
+                intent.putExtra("foodName","Tomato");
+                intent.putExtra("itemPrice", 15000);
+                intent.putExtra("itemDescription", "Fruits that are considered vegetables by nutritionists. Botanically, a fruit is a ripened flower ovary and contains seeds.");
+                startActivity(intent);
+            }
+        });
+
+        buttonPotato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StoreActivity.this, ItemActivity.class);
+                intent.putExtra("foodName","Potato");
+                intent.putExtra("itemPrice", 15000);
+                intent.putExtra("itemDescription", "A root vegetable. It is a small plant with large leaves. The part of the potato that people eat is a tuber that grows under the ground.");
+                startActivity(intent);
+            }
+        });
+
+        buttonCarrot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StoreActivity.this, ItemActivity.class);
+                intent.putExtra("foodName","Carrot");
+                intent.putExtra("itemPrice", 15000);
+                intent.putExtra("itemDescription", "A root vegetable, usually orange in colour, though purple, black, red, white, and yellow cultivars exist.");
+                startActivity(intent);
+            }
+        });
+
+        buttonLettuce.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StoreActivity.this, ItemActivity.class);
+                intent.putExtra("foodName","Lettuce");
+                intent.putExtra("itemPrice", 15000);
+                intent.putExtra("itemDescription", "An annual plant of the daisy family, Asteraceae. It is most often grown as a leaf vegetable, but sometimes for its stem and seeds.");
+                startActivity(intent);
+            }
+        });
+
+        buttonEggplant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StoreActivity.this, ItemActivity.class);
+                intent.putExtra("foodName","Eggplant");
+                intent.putExtra("itemPrice", 15000);
+                intent.putExtra("itemDescription", "A plant species in the nightshade family Solanaceae. Solanum melongena is grown worldwide for its edible fruit.");
+                startActivity(intent);
+            }
+        });
     }
 
     public void setTransparentStatusBarOnly(Activity activity) {
